6f12c9d59d5f76566479d066218f1f1e208750fe
KWJ30 <gidtjrwns30@naver.com>
2024-12-02 14:35:50 +0900

    Update ReviewServiceImp.java

--- src/main/java/com/mbc/service/ReviewServiceImp.java
+++ src/main/java/com/mbc/service/ReviewServiceImp.java
@@ -61,9 +61,4 @@ public class ReviewServiceImp implements ReviewService {
 		return mapper.getListWithPaging(cri, uno);
 	}
 	
-	@Override
-	public List<ReviewVO> getReviewListByUno(Long uno) {
-		return mapper.getReviewListByUno(uno);
-	}
-	
 }
