2341f7a3a6158b80ba9b87bc6fdafe160ea44aa7
J0hyun <bboreber04@gmail.com>
2024-12-02 09:52:15 +0900

    review

--- src/main/java/com/mbc/service/ReviewService.java
+++ src/main/java/com/mbc/service/ReviewService.java
@@ -17,5 +17,9 @@ public interface ReviewService {
 	public int remove(Long rno);
 	
 	public List<ReviewVO> getList(Criteria cri, Long uno);
+
 	
+	public List<ReviewVO> getReviewListWithProductAndbuyerIdAndsellerId();
+	
+	public List<ReviewVO> getReviewListByUno(Long uno);
 }
